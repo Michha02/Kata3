0077a292ee265a2572451d814894a1b347cd72b1
Michha02 <michha02@gmail.com>
2018-11-05 03:03:40 +0000

    Versión 1

---
+++ src/HistogramDisplay.java
@@ -0,0 +1,41 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartPanel;
+import java.awt.Dimension;
+import org.jfree.chart.JFreeChart;
+
+class HistogramDisplay extends ApplicationFrame{
+
+    HistogramDisplay(){
+        super("HISTOGRAMA");
+        setContentPane(createPanel());
+        pack();
+    }
+
+    void execute() {
+        setVisible(true);
+    }
+
+    private JPanel createPanel(){
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(500,400));
+        return chartPanel;
+    }
+
+    private JFreeChart createChart(DefaultCategoryDataset dataSet){
+        return ChartFactory.createBarChart("Histograma JFreeChart" , "Dominios email",
+                "Nº de emails", dataSet, PlotOrientation.VERTICAL, false, false,
+                rootPaneCheckingEnabled);
+    }
+
+    private DefaultCategoryDataset createDataset(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(5,"","gmail.com");
+        dataSet.addValue(7,"","ulpgc.es");
+        dataSet.addValue(3,"","outlook.com");
+        return dataSet;
+    }
+}
